97a79de148cdbd2ba058b9e9b656b8feb1884009
Marcos Delgado <marcosdelgado0408@gmail.com>
2021-03-01 22:43:26 -0300

    using mutual exclusion on threads

--- src/Point.java
+++ src/Point.java
@@ -11,48 +11,53 @@ public class Point {
     public double val;
     public double x, y; // coordenada do ponto
     public double distance;
-    public double media;
+    Object lock;
+
 
     public Point(double x, double y, double val) {
         this.x = x;
         this.y = y;
         this.val = val;
+        this.lock = new Object();
     }
 
     public Point() {
         this.x = 0;
         this.y = 0;
         this.val = 0;
+        this.lock = new Object();
     }
 
 
     public double classifyPoint(Point[] arr, int dataPointsNumber, int k, Point point){ // metodo para classificação de um ponto desconhecido p
 
-        Thread threadDistEucl = new Thread(){
-            @Override
-            public void run(){
-                for(int i=0;i<dataPointsNumber;i++){
-                    arr[i].distance = Math.sqrt((arr[i].x - point.x) * (arr[i].x - point.x) + (arr[i].y - point.y) * (arr[i].y - point.y)); // calculo distância euclidiana
-                }
-            }
-        };
-        threadDistEucl.start();
 
 
-        Thread comparator =  new Thread(){
-            @Override
-            public void run(){
+            Thread threadDistEucl = new Thread( ()-> {
 
-                Arrays.sort(arr, new Comparator<Point>() { // ordenar o array por distância -> crescente
-                    @Override
-                    public int compare(Point o1, Point o2) {
-                        return Double.compare(o1.distance, o2.distance);
+                synchronized (this){
+                    for(int i=0;i<dataPointsNumber;i++){
+                        arr[i].distance = Math.sqrt((arr[i].x - point.x) * (arr[i].x - point.x) + (arr[i].y - point.y) * (arr[i].y - point.y)); // calculo distância euclidiana
                     }
-                });
+                }
 
-            }
-        };
-        comparator.start();
+
+            });
+
+
+
+            Thread comparator = new Thread( () -> {
+
+                synchronized (this){
+                    Arrays.sort(arr, new Comparator<Point>() { // ordenar o array por distância -> crescente
+                        @Override
+                        public int compare(Point o1, Point o2) {
+                            return Double.compare(o1.distance, o2.distance);
+                        }
+                    });
+                }
+
+            });
 
 
 
@@ -61,20 +66,29 @@ public class Point {
          * */
 
 
-        Thread media1 =  new Thread(){
-            @Override
-            public void run(){
 
-                double media = 0;
-                for(int i=0;i<k;i++){
-                    media += arr[i].val; // media dos precos
+            Thread fazerMedia = new Thread( ()->{
+
+                synchronized (this){
+                    double media = 0;
+                    for(int i=0;i<k;i++){
+                        media += arr[i].val; // media dos precos
+                    }
+
+                    media = media / k;
+                    System.out.println(media);
                 }
 
-                media = media / k;
-                System.out.println(media);
-            }
-        };
-        media1.start();
+            });
+
+
+
+        threadDistEucl.start();
+        comparator.start();
+        fazerMedia.start();
+
+
+
 
         return 0;
 
